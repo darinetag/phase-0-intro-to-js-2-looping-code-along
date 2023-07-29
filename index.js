3264e6583d9ec0fc0cf6657a47e63f89512c9bc1
darinetag <tagdarinefatma@gmail.com>
2023-07-29 10:52:46 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, gift) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  module.exports = {
+    writeCards,
+  };
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
